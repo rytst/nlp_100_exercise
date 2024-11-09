ba13a4f239ab46d6a253ca213116f0ed1f9c12ad
rytst <r.saito0111@gmail.com>
2024-11-09 15:52:49 +0900

    exercise00

---
+++ ch01/exercise00.py
@@ -0,0 +1,41 @@
+# Chapter 01
+#
+# Exercise 00
+#
+
+import sys
+
+
+# Reverse string
+def reverse_str(input_str):
+
+    length = len(input_str)
+
+    last_index = length - 1
+
+    output_str = []
+    for i in range(length):
+        output_str.append(input_str[last_index-i])
+
+    return ''.join(output_str)
+        
+
+
+def main():
+    args = sys.argv
+
+    # Number of command line argments must be 1
+    if len(args) != 2:
+        print("Usage:\n", args[0], "string")
+        return
+
+    input_str = args[1] 
+
+    output_str = reverse_str(input_str)
+    print(output_str)
+
+
+
+
+if __name__ == "__main__":
+    main()
