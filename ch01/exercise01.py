0d8e285543d3fbe3619092e6701e2a43afef24b9
rytst <r.saito0111@gmail.com>
2024-11-09 16:07:27 +0900

    exercise01

---
+++ ch01/exercise01.py
@@ -0,0 +1,27 @@
+# Chapter 01
+#
+# Exercise 01
+#
+
+
+def extract_str(input_str):
+
+    length = len(input_str)
+
+    output_length = int(length / 2)
+    output_str = []
+    for i in range(output_length):
+        output_str.append(input_str[2 * i])
+
+    return ''.join(output_str)
+
+
+def main():
+    merged_str = "パタトクカシーー"
+
+    output_str = extract_str(merged_str)
+    print(output_str)
+
+
+if __name__ == "__main__":
+    main()
