6918eb81f2701a3fc8f507dafefc8821192baf96
rytst <r.saito0111@gmail.com>
2024-11-20 03:58:37 +0900

    exercise11

---
+++ ch02/exercise11.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+#
+# Chapter 02
+#
+# Exercise 11
+#
+
+import sys
+
+INPUT_FILE = "popular-names.txt"
+
+def main():
+    try:
+        fp = open(INPUT_FILE, 'r')
+    except OSError:
+        print("Could not open/read file:", input_file)
+        sys.exit()
+
+    with fp:
+        # The readlines() read all the lines in a single go and
+        # then return them as each line is a string element in a list
+        for line in fp.readlines():
+            line = line.replace('\t', ' ')
+            print(line, end='')
+
+
+
+if __name__ == "__main__":
+    main()
