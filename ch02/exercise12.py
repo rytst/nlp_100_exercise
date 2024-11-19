398a2fd934fc8de3727adf587cc882596af1548e
rytst <r.saito0111@gmail.com>
2024-11-20 04:46:10 +0900

    exercise12

---
+++ ch02/exercise12.py
@@ -0,0 +1,37 @@
+#!../venv/bin/python
+
+#
+# Chapter 02
+#
+# Exercise 12
+#
+
+import sys
+import polars as pl
+
+INPUT_FILE = "popular-names.txt"
+
+def main():
+
+    try:
+        df = pl.read_csv(INPUT_FILE, separator='\t')
+    except OSError:
+        print("Could not open/read file:", INPUT_FILE)
+        sys.exit()
+
+    for i in range(2):
+
+        col = df.select(pl.nth(i))
+
+        OUTPUT_FILE = "col{}.txt".format(i+1)
+        try:
+            fp = open(OUTPUT_FILE, 'w')
+        except OSError:
+            print("Could not open/read file:", OUTPUT_FILE)
+
+        col.write_csv(OUTPUT_FILE)
+
+
+
+if __name__ == "__main__":
+    main()
--- ch02/exercise12.sh
+++ ch02/exercise12.sh
@@ -6,5 +6,5 @@
 # Exercise 12
 #
 
-awk '{ print $1 }' popular-names.txt >col1.txt
-awk '{ print $2 }' popular-names.txt >col2.txt
+awk '{ print $1 }' popular-names.txt >col1_sh.txt
+awk '{ print $2 }' popular-names.txt >col2_sh.txt
