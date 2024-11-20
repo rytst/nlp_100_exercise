d0f60538e66a973c0eb2d7a17555992b688e57fb
rytst <r.saito0111@gmail.com>
2024-11-21 05:50:06 +0900

    refactor

--- ch02/exercise12.py
+++ ch02/exercise12.py
@@ -13,6 +13,8 @@ INPUT_FILE = "popular-names.txt"
 
 
 def main():
+
+    # Try to read file
     try:
         df = pl.read_csv(INPUT_FILE, separator="\t")
     except OSError:
@@ -22,12 +24,15 @@ def main():
     for i in range(2):
         col = df.select(pl.nth(i))
 
+        # col1.txt and col2.txt
         OUTPUT_FILE = "col{}.txt".format(i + 1)
+
         try:
             open(OUTPUT_FILE, "w")
         except OSError:
-            print("Could not open/read file:", OUTPUT_FILE)
+            print("Could not open/write file:", OUTPUT_FILE)
 
+        # Write data to OUTPUT_FILE
         col.write_csv(OUTPUT_FILE)
 
 
--- ch02/exercise13.py
+++ ch02/exercise13.py
@@ -10,10 +10,14 @@ import sys
 import polars as pl
 
 INPUT_FILES = ["col1.txt", "col2.txt"]
+OUTPUT_FILE = "merged.txt"
 
 
 def main():
+
+    # Initialization DataFrame
     df_left = pl.DataFrame({})
+
     for i in range(2):
         try:
             df_right = pl.read_csv(INPUT_FILES[i], separator="\t")
@@ -23,12 +27,12 @@ def main():
 
         df_left = df_left.with_columns(df_right)
 
-    OUTPUT_FILE = "merged.txt"
     try:
         open(OUTPUT_FILE, "w")
     except OSError:
-        print("Could not open/read file:", OUTPUT_FILE)
+        print("Could not open/write file:", OUTPUT_FILE)
 
+    # Write data to OUTPUT_FILE
     df_left.write_csv(OUTPUT_FILE, separator="\t")
 
 
