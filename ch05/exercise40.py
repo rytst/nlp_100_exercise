5cbd55075e0a0f8bf90e6cec84734652dfb3d976
rytst <r.saito0111@gmail.com>
2024-12-06 16:01:25 +0900

    refactor

--- ch05/exercise40.py
+++ ch05/exercise40.py
@@ -12,9 +12,9 @@ import sys
 class Morph:
     def __init__(self, surface, base, pos, pos1):
         self.surface = surface
-        self.base    = base
-        self.pos     = pos
-        self.pos1    = pos1
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
 
 
 def main():
@@ -33,11 +33,11 @@ def main():
     morph_list = list()
     with fp:
         for line in fp.readlines():
-            line = line.strip() # Remove new line
+            line = line.strip()  # Remove new line
             if line[0] == "*":
                 continue
 
-            token_and_info = line.split() # Split by space
+            token_and_info = line.split()  # Split by space
 
             token = token_and_info[0]
             if token == "EOS":
@@ -46,20 +46,19 @@ def main():
 
             # Get information from text data
             surface = token
-            base    = token_and_info[1][-3]
-            pos     = token_and_info[1][0]
-            pos1    = token_and_info[1][1]
+            base = token_and_info[1][-3]
+            pos = token_and_info[1][0]
+            pos1 = token_and_info[1][1]
 
+            # Create instance
             morph = Morph(surface, base, pos, pos1)
             morph_list.append(morph)
 
+    # Print expected data
     for morph in morph_list:
         if morph == "EOS":
             continue
-        print(morph.surface,":    " ,morph.pos)
-
-
-
+        print(morph.surface, ":    ", morph.pos)
 
 
 if __name__ == "__main__":
