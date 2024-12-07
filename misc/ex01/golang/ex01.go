bfbdde7579d5e833a41bb8b41d0fa2816cb3f705
rytst <r.saito0111@gmail.com>
2024-12-07 16:47:20 +0900

    add file

---
+++ misc/ex01/golang/ex01.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "log"
+    "os"
+    "strings"
+    "errors"
+)
+
+
+type nlp struct {
+    n int
+    l int
+    p int
+}
+
+
+func check(e error) {
+    if e != nil {
+        log.Fatal(e)
+        os.Exit(1)
+    }
+}
+
+func getIndex(line, c string) (int, error) {
+    idx := strings.Index(line, c)
+
+    if idx > -1 {
+        return idx, nil
+    }
+    return 0, errors.New("Invalid format")
+}
+
+func main() {
+    argsWithProg := os.Args
+
+    file_name := argsWithProg[1]
+    file, err := os.Open(file_name)
+    check(err)
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+
+    data := nlp{0, 0, 0}
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        n_idx, err := getIndex(line, "N")
+        check(err)
+        data.n += n_idx + 1
+        l_idx, err := getIndex(line, "L")
+        check(err)
+        data.l += l_idx + 1
+        p_idx, err := getIndex(line, "P")
+        check(err)
+        data.p += p_idx + 1
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(1 * data.n + 2 * data.l + 3 * data.p)
+}
