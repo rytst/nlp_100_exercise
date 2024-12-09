d26022a6f56d22fb6afd10a5fcde89b0f2a628cf
rytst <r.saito0111@gmail.com>
2024-12-09 16:19:25 +0900

    golang format

--- misc/ex01/golang/ex01.go
+++ misc/ex01/golang/ex01.go
@@ -1,66 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "bufio"
-    "log"
-    "os"
-    "strings"
-    "errors"
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
-
 type nlp struct {
-    n int
-    l int
-    p int
+	n int
+	l int
+	p int
 }
 
-
 func check(e error) {
-    if e != nil {
-        log.Fatal(e)
-        os.Exit(1)
-    }
+	if e != nil {
+		log.Fatal(e)
+		os.Exit(1)
+	}
 }
 
 func getIndex(line, c string) (int, error) {
-    idx := strings.Index(line, c)
+	idx := strings.Index(line, c)
 
-    if idx > -1 {
-        return idx, nil
-    }
-    return 0, errors.New("Invalid format")
+	if idx > -1 {
+		return idx, nil
+	}
+	return 0, errors.New("Invalid format")
 }
 
 func main() {
-    argsWithProg := os.Args
+	argsWithProg := os.Args
 
-    file_name := argsWithProg[1]
-    file, err := os.Open(file_name)
-    check(err)
-    defer file.Close()
+	file_name := argsWithProg[1]
+	file, err := os.Open(file_name)
+	check(err)
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-    data := nlp{0, 0, 0}
-    for scanner.Scan() {
-        line := scanner.Text()
+	data := nlp{0, 0, 0}
+	for scanner.Scan() {
+		line := scanner.Text()
 
-        n_idx, err := getIndex(line, "N")
-        check(err)
-        data.n += n_idx + 1
-        l_idx, err := getIndex(line, "L")
-        check(err)
-        data.l += l_idx + 1
-        p_idx, err := getIndex(line, "P")
-        check(err)
-        data.p += p_idx + 1
-    }
+		n_idx, err := getIndex(line, "N")
+		check(err)
+		data.n += n_idx + 1
+		l_idx, err := getIndex(line, "L")
+		check(err)
+		data.l += l_idx + 1
+		p_idx, err := getIndex(line, "P")
+		check(err)
+		data.p += p_idx + 1
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println(1 * data.n + 2 * data.l + 3 * data.p)
+	fmt.Println(1*data.n + 2*data.l + 3*data.p)
 }
