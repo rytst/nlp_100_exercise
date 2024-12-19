2bf4612c12a984f1cd8d2500af1f8a3372d229da
rytst <r.saito0111@gmail.com>
2024-12-19 09:24:21 +0900

    exercise56

--- ch06/exercise52_53_54_55_56.py
+++ ch06/exercise52_53_54_55_56.py
@@ -3,18 +3,47 @@
 #
 # Chapter 06
 #
-# Exercise 52 53
+# Exercise 52 53 54 55 56
 #
 
 import polars as pl
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 
 TRAIN_FILE = "./data/train.feature.txt"
 VALID_FILE = "./data/valid.feature.txt"
 TEST_FILE  = "./data/test.feature.txt"
+
+def wall():
+    print("=====================================================================")
+
+def line():
+    print("---------------------------------------------------------------------")
+
+def show_top10(coef, enc, train):
+    train.columns
+    for idx, label in enumerate(enc.inverse_transform([0,1,2,3])):
+        print("top10 of", label)
+        top10_name = np.array(train.columns)[np.argsort(coef[idx])[::-1]][:10]
+        top10      = np.sort(coef[idx])[::-1][:10]
+        for name, val in zip(top10_name, top10):
+            print(name, ":", val)
+        line()
+
+
+
+def show_bottom10(coef, enc, train):
+    train.columns
+    for idx, label in enumerate(enc.inverse_transform([0,1,2,3])):
+        print("top10 of", label)
+        top10_name = np.array(train.columns)[np.argsort(coef[idx])][:10]
+        top10      = np.sort(coef[idx])[:10]
+        for name, val in zip(top10_name, top10):
+            print(name, ":", val)
+        line()
+
 
 def main():
     try:
@@ -60,12 +89,14 @@ def main():
     )
     test  = test.drop(["URL", "PUBLISHER", "CATEGORY", "STORY", "HOSTNAME", "TIMESTAMP"])
 
+    # Label Encoding
     for label, idx in zip(list(enc.inverse_transform([0,1,2,3])), [0,1,2,3]):
         print(label, ":", idx)
     clf = LogisticRegression(random_state=42)
 
     # Exercise52
     clf.fit(train, y_train)
+    wall()
 
     # Exercise53
     pred_proba = clf.predict_proba(train)
@@ -76,15 +107,30 @@ def main():
     for pred, proba in zip(preds, probas):
         print(enc.inverse_transform([pred])[0], ":", proba)
 
+    wall()
+
 
     # Exercise54
     print("Training data:", clf.score(train, y_train))
     print("Test data:", clf.score(test, y_test))
+    wall()
 
     # Exercise55
     pred = clf.predict(test)
     print("Confusion Matrix")
     print(confusion_matrix(y_test, pred))
+    wall()
+
+    # Exercise56
+    report = classification_report(y_test, pred)
+    print(report)
+    wall()
+
+    # Exercise57
+    coef = clf.coef_
+    show_top10(coef, enc, train)
+    show_bottom10(coef, enc, train)
+    wall()
 
 
 if __name__ == "__main__":
