a78bf87f1b314d3a08f0b05ec91c8c4cf221742f
rytst <r.saito0111@gmail.com>
2025-01-09 09:26:14 +0900

    exercise62

---
+++ ch07/exercise62.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+#
+# Chapter 07
+#
+# Exercise 62
+#
+
+from gensim import models
+# from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+def main():
+
+    try:
+        wv = models.KeyedVectors.load_word2vec_format(
+            './GoogleNews-vectors-negative300.bin',
+            binary=True,
+        )
+    except:
+        print("Could not read file")
+        sys.exit()
+
+    res = wv.most_similar(positive=["United_States"])[:10]
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
